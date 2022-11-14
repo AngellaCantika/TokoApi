448b2ef6073a1319e364d572d46d4a4fa8595cb2
AngellaCantika <rangellaoktacantika@gmail.com>
2022-11-14 21:07:30 +0700

    menambah login member dan produk

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,12 +6,12 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    protected function responseHasil($code,$status,$data)
-{
-    return \response()->json([
+    protected function responseHasil($code, $status, $data)
+    {
+      return \response()->json([
         'code' => $code,
         'status' => $status,
         'data' => $data,
     ]);
-}
+  }
 }
